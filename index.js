ccb28f157fa21b6863ba619d7cec9593f2f2e42f
sriram1702 <ramsri1702@gmail.com>
2022-01-25 16:34:54 +0530

    rest api completed

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express=require('express')
 const mongoose=require('mongoose')
 const bodyparser = require('body-parser');
+const morgan = require('morgan')
+const Joi=require('Joi')
 const userservices=require('./routes/user_services');
 const app=express()
 
@@ -8,11 +10,15 @@ const app=express()
 mongoose.connect('mongodb://localhost/user-services')
 .then(()=>console.log('Connected to mongodb'))
 .catch(err=>console.error('could not connect to mongodb'))
-app.use('/userservices',userservices);
-app.use(express.json())
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({     // to support URL-encoded bodies
-    extended: true
-  }));
+  extended: true
+}));
+app.use('/userservices',userservices);
+app.use(express.json())
+
+app.use(morgan('tiny'))
+
 const port=process.env.PORT||3000;
 app.listen(port,()=>console.log(`Listening on port ${port}`));
+
--- routes/user_services.js
+++ routes/user_services.js
@@ -1,6 +1,20 @@
 const express=require('express')
 const mongoose=require('mongoose')
 const router=express.Router();
+const Joi=require('joi');
+const { string } = require('joi');
+const schema = Joi.object({
+    first_name:Joi.string().min(3).required(),
+    last_name:Joi.string().min(3).required(),
+    email_id: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
+
+    phonenumber:Joi.string().max(10).pattern(/^[0-9]+$/).required(),
+});
+
+// const {userValidation}  = require("./validator")
+
+
+
 
 const userSchema= new mongoose.Schema({
     first_name:{
@@ -31,88 +45,170 @@ const userSchema= new mongoose.Schema({
     },
     phonenumber:{
         type: Number,
-        validate: {
-            validator: function(v) {
-                return /d{10}/.test(v);
-            },
-            message: '{VALUE} is not a valid 10 digit number!'
-        }
+        
+        // validate: {
+        //     validator: function(v) {
+        //         return /d{10}/.test(v);
+        //     },
+        //     message: '{VALUE} is not a valid 10 digit number!'
+        // }
     }
 }) 
 
 const Users= mongoose.model('Users',userSchema)
-router.get('/',async(req,res)=>{
-    
-    const users=await Users.find().sort('first_name')
-    res.send(users);
-});
+// const doc=new mongoose.Model();
+// doc._id instanceof mongoose.Types.ObjectId;
+router.post('/tocreateauser',async(req,res)=>{
+    const {error}=schema.validate(req.body)
 
-
-router.post('/',async(req,res)=>{
+    if(error) {
+        return res.status(400).send(error.details[0].message);
+    }
     let users=new Users({
         first_name:req.body.first_name,
         last_name:req.body.last_name,
         email_id:req.body.email_id,
         phonenumber:req.body.phonenumber
     })
-    if(!users) return res.status(404).send('error occurred pls try again')
+    //  if(!users) return res.status(404).send('error occurred pls try again')
     try {
-        users=await users.save();
-        res.status(200).json(users)
+       users=await users.save();
+        res.status(200).send(users)
     } catch (error) {
-        
+        res.status(500).send('some error occurred need to check').write('whatsapp')
+    
         console.log(error);
+     
     }
     
        
     
     
-        res.status(404).send(error.message)
+    //  res.status(404).send(error.message).write("something happened")
+    // res.send('helllo world')
+    // res.write('hey its works')
     
    
     
 })
+router.get('/',async(req,res)=>{
+    //   if(req.url!='/'){
+    //     res.send("enter the correct url")
+    //     res.write("enter the correct url write")
+        
+    // }
+  
+    if(req.url!='/') return res.status(500).send("pls check the url")
+    const users=await Users.find().sort('first_name')
+    if(!users) return res.status(404).send('error occurred pls try again')
+    res.send(users);
+  
+});
+
+
+// router.post('/tocreateauser',async(req,res)=>{
+//     const {error}=schema.validate(req.body)
+
+//     if(error) {
+//         return res.status(400).send(error.details[0].message);
+//     }
+//     let users=new Users({
+//         first_name:req.body.first_name,
+//         last_name:req.body.last_name,
+//         email_id:req.body.email_id,
+//         phonenumber:req.body.phonenumber
+//     })
+//     //  if(!users) return res.status(404).send('error occurred pls try again')
+//     try {
+//        users=await users.save();
+//         res.status(200).send(users)
+//     } catch (error) {
+//         res.status(500).send('some error occurred need to check').write('whatsapp')
+    
+//         console.log(error);
+     
+//     }
+    
+       
+    
+    
+//     //  res.status(404).send(error.message).write("something happened")
+//     // res.send('helllo world')
+//     // res.write('hey its works')
+    
+   
+    
+// })
 
 
 router.put('/:id',async(req,res)=>{
+
+    const {error}=schema.validate(req.body)
+
+    if(error) {
+        return res.status(400).send(error.details[0].message);
+    }
+      
   const users=await  Users.findByIdAndUpdate(req.params.id,{first_name:req.body.first_name,
         last_name:req.body.last_name,
         email_id:req.body.email_id,
         phonenumber:req.body.phonenumber},{new:true})
-        if(!users) return res.status(404).send('error occurred pls try again')
-   
+        
+          
+        if(!users) return res.status(404).send('the user with the given id is not found')
+   try{
         res.status(200).send(users)
-   
-        res.send(error)
+   }
+   catch(error){
+    res.status(500).send('some error occurred need to check')
+
+   }
+        // res.send(error)
         
     
 })
 
-router.delete('/',async (req,res)=>{
+
+router.delete('/:id',async (req,res)=>{
+   
     const users=await Users.findByIdAndRemove(req.params.id)
     if(!users) return res.status(404).send('error occurred pls try again')
-   
+   try{
         res.status(200).send(users)
-  
-        res.status(404).send(error)
-    
+   }
+   catch(err){
+
+   
+        res.status(500).send('some error occured need to check')
+   }
 
 })
 
 
-router.get('/',async(req,res)=>{
+router.get('/:id',async(req,res)=>{
+   
     const users=await Users.findById(req.params.id);
     if(!users) return res.status(404).send('error occurred pls try again')
-    
+    try {
         res.status(200).send(users)
+    } catch (error) {
+        res.status(404).send('some error occured need to check')
+    }
+       
   
-        res.status(404).send(error)
+       
     
     
 })
-
-
-
+router.patch('/:id',async(req,res)=>{
+    const users=await  Users.findByIdAndUpdate(req.params.id,{email_id:req.body.email_id
+     
+        },{new:true})
+        if(!users) return res.status(404).send('error occurred pls try again')
+   
+        res.status(200).send(users)
+    
+})
 
 
 
